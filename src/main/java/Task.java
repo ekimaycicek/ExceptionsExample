fc32f03b00c9804ef4cc081646dea591bc7e7ce7
ekimaycicek <ekimaycicek@icloud.com>
2023-10-19 20:47:37 +0300

    new

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,7 +6,6 @@ public class Task {
         Task -> Girilen password'un 6 karakterden az 10 karakterden fala olmamasını kontrol eden code create ediniz
          */
 
-
         Scanner sc = new Scanner(System.in);
         System.out.print("enter password : ");
 
